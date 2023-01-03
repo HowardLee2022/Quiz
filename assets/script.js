c373d6d1f7b71b6c5fdd98c9682da9cd6af50027
howard <howardnlee94@gmail.com>
2023-01-02 18:43:48 -0800

    updated quiz questions

--- assets/script.js
+++ assets/script.js
@@ -10,11 +10,13 @@ var container = document.querySelector(".container");
 // var score1= document.getElementById('score');
 // var score2= 0;
 
-var questionList=["1+1","What is my name?","3+3"];
+var questionList=["1+1","What is my name?","3+3","What is my Last Name?","What state am I from?"];
 var answerList=[[2,4,5,6],
                 ["howard","jackie","Howard","Jackie"],
-                [3,6,5,10]];
-var correctList=[0,2,1];
+                [3,6,5,10],
+                ["Lee","le","li","ly"],
+                ["Texas","California","Oregon","Washington"]];
+var correctList=[0,2,1,0,3];
 
 function setTime(){
     var timerInterval = setInterval(function(){
@@ -42,60 +44,28 @@ function question1(event){
             changeQuestion();
             
             console.log(count);
-            
         }else{
+            Timeleft= Timeleft - 10;
             console.log("wrong");
             count++
             changeQuestion();
             
         }
-    //         console.log("nice");
-    //         question.textContent= questionList[i];
-    //         answer1.textContent=answerList[i][0];
-    //         answer2.textContent=answerList[i][1];
-    //         answer3.textContent=answerList[i][2];
-    //         answer4.textContent=answerList[i][3];
-    //         i++;
-    //     }else{
-    //         console.log("wrong")
-    //         question.textContent= questionList[i];
-    //         answer1.textContent=answerList[i][0];
-    //         answer2.textContent=answerList[i][1];
-    //         answer3.textContent=answerList[i][2];
-    //         answer4.textContent=answerList[i][3];
-    //         i++;
-    //     }
     }
 
-
-    // if(element.matches(".box") )   
-    //     clickBtn = element.getAttribute("data-number");
-    //     console.log(element);
-    //     console.log(clickBtn);
-    //     if(event.target.getAttribute("data-number")==correctList[0]){
-    //         console.log("cool");
-    //         changeQuestion();
-    //         // container.addEventListener("click",question2);
-    //     }else{  Timeleft--;
-    //         console.log("wrong");
-    //         changeQuestion();
-    //         // container.addEventListener("click",question2);
-    //     }   
 
 
 
 function changeQuestion(){
-    // question.textContent= questionList[1];
-    // answer1.textContent=answerList[1][0];
-    // answer2.textContent=answerList[1][1];
-    // answer3.textContent=answerList[1][2];
-    // answer4.textContent=answerList[1][3];
-    question.textContent= questionList[count];
-   answer1.textContent=answerList[count][0];
-   answer2.textContent=answerList[count][1];
-   answer3.textContent=answerList[count][2];
-   answer4.textContent=answerList[count][3];
-    
+    if(count>questionList.length){
+        console.log("end");
+    }else{
+        question.textContent= questionList[count];
+        answer1.textContent=answerList[count][0];
+        answer2.textContent=answerList[count][1];
+        answer3.textContent=answerList[count][2];
+        answer4.textContent=answerList[count][3]; 
+    }
 }
 
 
@@ -129,51 +99,3 @@ function startquiz(){
 
 
 
-// event.stopPropagation();
-
-//   container.addEventListener("click", function(){
-//         for(i=0, i < questionList.length, i++) {
-//             question.textContent= questionList[i];
-//             answer1.textContent=answerList[i][0];
-//             answer2.textContent=answerList[i][1];
-//             answer3.textContent=answerList[i][2];
-//             answer4.textContent=answerList[i][3];
-//             var element = event.target;
-//             howard = element.getAttribute("data-number");
-//             console.log(howard)
-//             console.log(element)
-
-
-//__________________________________________________________________________________
-// function question2(event){
-//     question.textContent= questionList[1];
-//     answer1.textContent=answerList[1][0];
-//     answer2.textContent=answerList[1][1];
-//     answer3.textContent=answerList[1][2];
-//     answer4.textContent=answerList[1][3];
-//     var element = event.target;
-//     if(element.matches(".box"))   
-//         clickBtn = element.getAttribute("data-number");
-//         console.log(clickBtn);
-//         if(clickBtn==correctList[1]){
-//             console.log("nice"); 
-//             // container.addEventListener("click", question3(event));    
-//         }
-// }
-
-
-// function question3(event){
-//     question.textContent= questionList[2];
-//     answer1.textContent=answerList[2][0];
-//     answer2.textContent=answerList[2][1];
-//     answer3.textContent=answerList[2][2];
-//     answer4.textContent=answerList[2][3];
-//     var element = event.target;
-//     console.log(element);
-//     if(element.matches(".box"))   
-//         howard = element.getAttribute("data-number");
-//         console.log(howard);
-//         if(howard==correctList[2]){
-//             console.log("nice");     
-//         }
-// }
